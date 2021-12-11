a436744f8e7f06137daa8af717393a9f8ecd3f4f
Saikat-Chowdhury90 <sonusaikat09@gmail.com>
2021-12-11 19:17:42 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
+import React,{useState , useEffect} from 'react';
 import './App.css';
-
+import quizData from './data/quiz.json';
+import {Questionaire} from './components';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [questions,setQuestions]= useState([]);
+  const [currentIndex, setCurrentIndex]=useState(0);
+  const [score , setScore] =useState(0);
+  const [showAnswers , setshowAnswers] = useState(false);
+  useEffect(() => {
+    
+    const questions = quizData.results.map((question) => 
+    ({
+      ...question,
+      answers: [
+        question.correct_answer,
+        ...question.incorrect_answers
+      ].sort(() => Math.random() - 0.5),
+    })
+    )
+    setQuestions(questions);
+  }, []);
+
+  
+  //console.log(quizData.results);
+  // let max=10;
+  // let i = Math.floor(Math.random() * max);
+
+const handleAnswer = (answer) => {
+
+  // const newIndex = currentIndex + 1
+  // setCurrentIndex(currentIndex + 1);
+if(!showAnswers)
+{
+  if(answer === questions[currentIndex].correct_answer){
+    setScore(score +1);
+  }
 }
+ 
+  setshowAnswers(true);
+  
+
+};
+
+const handleNextQuestion = () => {
+  setshowAnswers(false);
+
+  setCurrentIndex(currentIndex+1);
+}
+
+      return questions.length > 0 ? (
+         
+        <div className="container">
+          {currentIndex >= questions.length ? (
+
+         <h1 className="text-3xl text-white font-bold">Your Knowledge : {score}</h1>
+        
+      ) :(
+        <Questionaire data={questions[currentIndex]} handleNextQuestion={handleNextQuestion} showAnswers={showAnswers} handleAnswer={handleAnswer} /> 
+      )}
+      </div> ) : (
+        <h1 className="text-2xl text-white font-bold">Loading...</h1>
+      );
+      
+      
+    
+  
+};
+
+
 
 export default App;
---
+++ src/components/Questionaire.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+
+
+const Questionaire = ({
+    showAnswers,
+    handleAnswer,
+    handleNextQuestion,
+    data: {question, correct_answer , answers},
+}) => {
+
+
+
+return(
+<div>
+    <div className="bg-white text-black p-10 rounded-lg shadow-md  ">
+        <h2 className="font-bold text-2xl" dangerouslySetInnerHTML={{__html:question}}/>
+    </div>
+    <div className="flex flex-wrap mt-4 justify-between ">
+      {answers.map((answer , idx) => {
+        const bgColor = showAnswers ? answer === correct_answer ? 'bg-green-500'
+        : 'bg-red-500':'bg-white';
+
+        const textColor = showAnswers ? 'text-white' : 'text-black';
+        return (
+         <button
+                key={idx}
+              className={` ${bgColor} ${textColor} p-4  font-semibold rounded shadow w-5/12 mb-4 `}
+
+              onClick={() => handleAnswer(answer)}>
+                  {answer}
+          </button>
+         );
+     })
+
+      }
+
+    
+
+    </div>
+        
+    {showAnswers && (
+         <div className="flex items-center justify-center">
+         <button onClick={handleNextQuestion}
+         className={` bg-black border-2 border-white hover:bg-white hover:text-black hover:border-black text-white p-4  font-semibold rounded  w-5/12 mt-10   `} >
+           Next
+         </button>
+         </div>
+     )
+     
+     }
+      
+    
+</div>
+
+);
+};
+
+function  everydayIMShuffling(arr) 
+{
+    for (let i = 0; i < arr.length; i++) {
+      const idx1 = Math.floor(Math.random() * arr.length);
+      const idx2 = Math.floor(Math.random() * arr.length);
+
+      
+
+      
+    }
+}
+
+export default Questionaire;
+
